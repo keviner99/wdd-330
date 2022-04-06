6285b9f50d68d344380a87cefca5591bc6bc3c7a
Kevin Espinoza Reyes <90742482+keviner99@users.noreply.github.com>
2022-04-05 23:14:17 -0500

    updated the portfolio menu and added week 11 and 12

--- js/main.js
+++ js/main.js
@@ -41,10 +41,41 @@ const links = [{
     }
 ]
 
+const links2 = [{
+        label: "Week11 notes",
+        url: "week11/index.html"
+    },
+    {
+        label: "Week12 notes",
+        url: "week12/index.html"
+    },
+    {
+        label: "Week13 notes",
+        url: "week12/index.html"
+    }
+]
+
+
 function loadIndex() {
     const ol = document.querySelector("#linksList")
 
     links.forEach(link => {
+
+        const li = document.createElement("li");
+        const href = document.createElement("a");
+        href.setAttribute("href", link.url);
+        href.innerText = link.label;
+
+        li.appendChild(href);
+        ol.appendChild(li);
+    })
+
+}
+
+function loadIndex2() {
+    const ol = document.querySelector("#linksList2")
+
+    links2.forEach(link => {
 
         const li = document.createElement("li");
         const href = document.createElement("a");
