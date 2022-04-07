6451ea505c84b7b6316a02f9115d7e35ba117a3b
Kevin Espinoza Reyes <90742482+keviner99@users.noreply.github.com>
2022-04-07 00:51:43 -0500

    added finak project

---
+++ week12/js/places.js
@@ -0,0 +1,82 @@
+fetch((url = "json/places.json"))
+    .then((data) => data.json())
+    .then((places) => {
+        const randomNum = Math.floor(Math.random() * places.length);
+        const placeRandom = places[randomNum];
+
+        function randomizer() {
+            let random = places[Math.floor(Math.random() * places.length)];
+            nam.innerText = `${random.name}`;
+            city.innerText = random.city;
+            tourcost.innerText = random.tourcost;
+            nam.className = "nam";
+            city.className = "city";
+            tourcost.className = "tourcost";
+        }
+
+        randomizer();
+
+        document.querySelector("button").addEventListener("click", randomizer);
+    });
+
+// this is test code //
+
+fetch(url)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(jsonObject) {
+        console.table(jsonObject);
+
+        const places = jsonObject;
+        places.forEach((place) => {
+            let card = document.createElement("dl");
+            let name = document.createElement("dt");
+            let city = document.createElement("dd");
+            let tourcost = document.createElement("dd");
+
+
+
+            name.innerHTML = `${place.name}`;
+            city.innerHTML = `${place.city}`;
+            tourcost.innerHTML = `${place.tourcost}`;
+
+
+
+            card.appendChild(name);
+            card.appendChild(city);
+            card.appendChild(tourcost);
+
+
+
+            document.querySelector("div.cards").appendChild(card);
+        });
+    });
+
+
+
+
+
+const placebutton = document.getElementById("showrandomplace");
+placebutton.addEventListener("click", showrandom);
+
+function showrandom() {
+    var x = document.getElementById("randomplace");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+const listbutton = document.getElementById("listbutton");
+listbutton.addEventListener("click", showlist);
+
+function showlist() {
+    var x = document.getElementById("placeList");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
