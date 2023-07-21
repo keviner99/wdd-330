ee6c429ff26f518dbc27fe058d78f66823ade682
keviner99 <90742482+keviner99@users.noreply.github.com>
2023-07-21 12:39:42 -0500

    add quiz function

--- sitepoint/chapter 4/quiz/js/main.js
+++ sitepoint/chapter 4/quiz/js/main.js
@@ -5,17 +5,33 @@ const quiz = [
     ["What is Batman's real name?", "Bruce Wayne"]
 ];
 
-let score = 0 // initialize score
+function start(quiz) {
+    let score = 0;
 
-for (const [question, answer] of quiz) {
-    const response = prompt(question);
-    if (response === answer) {
-        alert('Correct!');
-        score++;
-    } else {
-        alert(`Wrong¡ The correct answer was ${answer}`);
+    //main game loop
+    for (const [question, answer] of quiz) {
+        const response = ask(question);
+        check(response, answer);
+    }
+    //end of main game loop
+    gameOver();
+
+    //function declarations
+    function ask(question) {
+        return prompt(question);
+    }
+
+    function check(response, answer) {
+        if (response === answer) {
+            alert('Correct!');
+            score++;
+        } else {
+            alert(`Wrong! The correct answer was ${answer}`);
+        }
+    }
+
+    function gameOver() {
+        alert(`Game Over, you scored ${score} points${score !== 1 ? 's' : ''}`);
     }
 }
-
-//At the end of the game, report the player's score
-alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+start(quiz);
